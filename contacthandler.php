295f5a30a4752c38604f834500971062a3ecd547
Lalit Rupani <56091708+lalitrupani@users.noreply.github.com>
2020-07-10 14:21:13 +0530

    Update contacthandler.php

--- contacthandler.php
+++ contacthandler.php
@@ -1,9 +1,9 @@
 
 
 <?php
-  $name = $_POST['contact_name'];
-  $visitor_email = $_POST['contact_email'];
-  $message = $_POST['contact_message'];
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
   $email_from = 'geek@dr.com';
   $email_body= "User Name: $name.\n". 
                  "User Email: $Visitor_email.\n".
@@ -12,7 +12,7 @@
 
   $to= "geek@dr.com";
   $headers = "From: $email_from \r\n";
-  $headers.= $Reply-To: $visitor_email;
+  $headers.= $Reply-To: $visitor_email; 
   
  mail($to,$email_Subject,$email_body,$headers);
 
