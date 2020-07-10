9c5062f5971d2a193cfa76605e118729c03d35c3
Lalit Rupani <56091708+lalitrupani@users.noreply.github.com>
2020-07-10 14:18:39 +0530

    Update contacthandler.php

--- contacthandler.php
+++ contacthandler.php
@@ -1,10 +1,21 @@
 
+
 <?php
-  $name = $_POST['name'];
-  $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
- $to= "geek@dr.com";
- mail($to);
+  $name = $_POST['contact_name'];
+  $visitor_email = $_POST['contact_email'];
+  $message = $_POST['contact_message'];
+  $email_from = 'geek@dr.com';
+  $email_body= "User Name: $name.\n". 
+                 "User Email: $Visitor_email.\n".
+                  "User Message: $message.\n";
+  $email_Subject= ' Form Submission from website';
+
+  $to= "geek@dr.com";
+  $headers = "From: $email_from \r\n";
+  $headers.= $Reply-To: $visitor_email;
+  
+ mail($to,$email_Subject,$email_body,$headers);
+
  header("Location: index.html");
 
 
