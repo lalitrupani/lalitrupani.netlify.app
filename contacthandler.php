4cb89e93ff1e1a0fc695528e2e07b3d933e8e605
Lalit Rupani <56091708+lalitrupani@users.noreply.github.com>
2020-07-10 14:44:53 +0530

    Update contacthandler.php

--- contacthandler.php
+++ contacthandler.php
@@ -3,17 +3,11 @@
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
   $message = $_POST['message'];
-  $email_from = 'geek@dr.com';
-  $email_body= "User Name: $name.\n". 
-                 "User Email: $visitor_email.\n".
-                  "User Message: $message.\n";
   $email_Subject= ' Form Submission from website';
-
+$txt= "Mail from".$name.".\n\n".$message;
   $to= "geek@dr.com";
-  $headers = "From:".$email_from\r\n;
- mail($to, $email_Subject, $email_body, $headers)
+  $headers = "From:".$visitor_email\r\n;
+ mail($to, $email_Subject, $txt, $headers);
 
- header("Location: index.html?mailsend"); 
-
-
+ header("Location: index.php?mailsend"); 
 ?>
